6bedf7eeda1d81595a069030dc13655feac20323
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-12 14:42:56 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -15,7 +15,7 @@ const handler = async (m, {conn}) => {
   });
 };
 handler.help = ['blocklist'];
-handler.tags = ['main'];
+handler.tags = ['owner'];
 handler.command = ['blocklist', 'listblock'];
 handler.rowner = true;
 export default handler;
