9b8214ebd83533f6d7714eb617c765b898d1639c
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-12 15:24:03 -0500

    Actualizar main-menuaudios.js

--- plugins/main-menuaudios.js
+++ plugins/main-menuaudios.js
@@ -83,3 +83,62 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
+
+
+// MENU OWNER BY DEV.CRISS
+
+const defaultMenu = {
+  before: (name, readMore) => ` ¡Hola *${name}*, aquí está mi menú de economía:\n\n${readMore}`,
+  header: category => `┏━━⪩「 *${category}* 」⪨`,
+  body: cmd => `┃ ${cmd}`,
+  footer: '┗━━━━━━━━━━━━━━━⪩',
+  after: ''
+}
+
+let handler = async (m, { conn, usedPrefix: _p }) => {
+  let name = await conn.getName(m.sender)
+  let tag = `@${m.sender.split('@')[0]}`
+  let tags = { owner: 'Owner' }
+  let imgPath = './src/catalogo.jpg'
+
+  let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => ({
+    help: Array.isArray(plugin.help) ? plugin.help : [plugin.help],
+    tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+  }))
+
+  let groups = {}
+  for (let tag in tags) {
+    groups[tag] = help.filter(plugin => plugin.tags.includes(tag))
+  }
+
+  const more = String.fromCharCode(8206)
+  const readMore = more.repeat(4001)
+
+  let text = [
+    defaultMenu.before(name, readMore),
+    ...Object.keys(tags).map(tagKey => {
+      return defaultMenu.header(tags[tagKey]) + '\n' + [
+        ...groups[tagKey].map(plugin =>
+          plugin.help.map(cmd =>
+            defaultMenu.body(_p + cmd)
+          ).join('\n')
+        ),
+        defaultMenu.footer
+      ].join('\n')
+    }),
+    defaultMenu.after
+  ].join('\n')
+
+  await m.react('💸')
+  await conn.sendMessage(m.chat, {
+    image: { url: imgPath },
+    caption: text,
+    mentions: [m.sender]
+  }, { quoted: m })
+}
+
+handler.help = ['menueconomia']
+handler.tags = ['rpg']
+handler.command = ['menue', 'menueco', 'menueconomia']
+
+export default handler
