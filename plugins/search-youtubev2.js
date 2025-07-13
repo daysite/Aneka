d44be75615072745c51352829b178e1f02af8536
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-13 17:54:54 -0500

    Actualizar search-youtubev2.js

--- plugins/search-youtubev2.js
+++ plugins/search-youtubev2.js
@@ -25,7 +25,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
   conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
 }
 
-handler.help = ['ytsearch'] 
+handler.help = ['yts2'] 
 handler.tags = ['search']
 handler.command = ['ytsearch2', 'yts2']
 
