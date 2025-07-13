8a097c802d18fea7ad2fd12d6125462aa319e4a1
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-13 14:15:52 -0500

    Actualizar sticker-fachero.js

--- plugins/sticker-fachero.js
+++ plugins/sticker-fachero.js
@@ -4,7 +4,7 @@ import cheerio from 'cheerio';
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) {
-        return conn.reply(m.chat, `*🍟 Ingresa un texto* *• Ejemplo :* ${usedPrefix + command} *Izumikzx*`, m, fake);
+        return conn.reply(m.chat, `*🍟 Ingresa un texto* *• Ejemplo :* ${usedPrefix + command} *Izumikzx*`, m, rcanal);
     }
 
     m.reply("Creando su Logo Porfavor Espere 🍟... ");
