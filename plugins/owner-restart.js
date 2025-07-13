c87fe829e13f66bbf36321224b033144d980e8c4
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-13 09:43:39 -0500

    Actualizar plugins

---
+++ plugins/owner-restart.js
@@ -0,0 +1,11 @@
+const handler = async (m, {conn, isROwner, text}) => {
+  if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
+    // conn.readMessages([m.key])
+    await m.reply('*❤️‍🩹 Reiniciando el bot...*\n\n*» Espere un momento para volver a usar el Bot, puede tomar unos segundos.*');
+    process.send('reset');
+};
+handler.help = ['restart'];
+handler.tags = ['owner'];
+handler.command = ['restart', 'reiniciar'];
+handler.rowner = true;
+export default handler;
