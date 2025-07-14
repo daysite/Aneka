17c79395a4921e64e16a719a18276e92e75cd3ea
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-13 19:44:47 -0500

    Actualizar emox-fuck2.js

--- plugins/emox-fuck2.js
+++ plugins/emox-fuck2.js
@@ -55,7 +55,7 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     }
 }
 
-handler.help = ['fuck2/coger2 @tag'];
+handler.help = ['fuck2'];
 handler.tags = ['emox'];
 handler.command = ['fuck2','coger2'];
 handler.group = true;
