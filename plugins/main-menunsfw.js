9649f11daa5783e0f88d8ca29956019cf1d4442a
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-13 19:26:11 -0500

    Actualizar main-menunsfw.js

--- plugins/main-menunsfw.js
+++ plugins/main-menunsfw.js
@@ -1,94 +1,60 @@
-import fetch from 'node-fetch';
 
-const handler = async (m, {conn, usedPrefix, text, isPrems}) => {
+let handler = async (m, { conn, usedPrefix: _p }) => {
+  let usertag = '@' + m.sender.split('@')[0]
+  const vid = ''
+  let tags = { nsfw: 'Nsfw' }
 
-  try {
-    const img = './src/catalogo.jpg';
-    const more = String.fromCharCode(8206);
-    const readMore = more.repeat(850);
-    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+  let defaultMenu = {
+    before: `ㅤᨦ۪۪۪۪ׄ᷼ㅤ֢ㅤׄㅤׅ֟፝ㅤ⋱ㅤ⁝ㅤ⋰ㅤׅ፝֟ㅤׄㅤ֢ㅤ۪۪۪۪ׄ᷼ഒ
+🔥 ׅ  *¡Hola! ¿Cómo estás?*  ৎ୭
+ׅ ෫${usertag}  ಒ
+‎ ‎ ‎ ‎౨ৎ  ‎ ‎ ‎ ‎*Bienvenido* ‎ ‎  ‎ ‎✿̮    ׅ  *al*   ୂ
+⿻     *𝖬𝖾𝗇𝗎*    ෨    *𝖭𝗌𝖿𝗐*    𑇙ᰍ
 
-    const str = `👋🏻 ¡Hᴏʟᴀ! ${taguser}
-Bɪᴇɴᴠᴇɴɪᴅᴏ ᴀʟ ᴍᴇɴᴜ *ɴsғᴡ* 🔞
-
-\`\`\`${fechaHora}\`\`\`
-
-𓂂𓏸  𐅹੭੭   *\`ᑲᥙsᥴᥲძ᥆rᥱs\`* 🪱 ᦡᦡ
-ര ׄ⃟🪱˚ .xnxxsearch *texto*
-ര ׄ⃟🪱˚ .xvsearch *texto*
-ര ׄ⃟🪱˚ .phsearch *texto*
-ര ׄ⃟🪱˚ .r34 *texto*
-
-𓂂𓏸  𐅹੭੭   *\`ძᥱsᥴᥲrgᥲs\`* 🧋ᦡᦡ
-ര ׄ⃟🧋˚ .xnxxdl *ulr*
-ര ׄ⃟🧋˚ .xvdl *url*
-ര ׄ⃟🧋˚ .phdl *url*
-
-𓂂𓏸  𐅹੭੭   *\`gі𝖿s\`* 🦪 ᦡᦡ
-ര ׄ⃟🦪˚ .follar *@tag*
-ര ׄ⃟🦪˚ .coger *@tag*
-ര ׄ⃟🦪˚ .coger2 *@tag*
-ര ׄ⃟🦪˚ .penetrar *@tag*
-ര ׄ⃟🦪˚ .anal *@tag*
-ര ׄ⃟🦪˚ .sexo *@tag*
-ര ׄ⃟🦪˚ .violar *@tag*
-ര ׄ⃟🦪˚ .rusa *@tag*
-ര ׄ⃟🦪˚ .sixnine *@tag*
-ര ׄ⃟🦪˚ .pies *@tag*
-ര ׄ⃟🦪˚ .mamada *@tag*
-ര ׄ⃟🦪˚ .lickpussy *@tag*
-ര ׄ⃟🦪˚ .grabboobs *@tag*
-ര ׄ⃟🦪˚ .suckboobs *@tag*
-ര ׄ⃟🦪˚ .cum *@tag*
-ര ׄ⃟🦪˚ .fap *@tag*
-ര ׄ⃟🦪˚ .manosear *@tag*
-ര ׄ⃟🦪˚ .lesbianas *@tag*
-
-𓂂𓏸  𐅹੭੭   *\`ᥴ᥆ᥒ𝗍ᥱᥒіძ᥆\`* 🍒 ᦡᦡ
-ര ׄ⃟🍒˚ .pack
-ര ׄ⃟🍒˚ .pack2
-ര ׄ⃟🍒˚ .pack3
-ര ׄ⃟🍒˚ .videoxxx
-ര ׄ⃟🍒˚ .videoxxx2
-ര ׄ⃟🍒˚ .randomxxx
-ര ׄ⃟🍒˚ .nsfwloli
-ര ׄ⃟🍒˚ .nsfwfoot
-ര ׄ⃟🍒˚ .nsfwass
-ര ׄ⃟🍒˚ .nsfwbdsm
-ര ׄ⃟🍒˚ .nsfwcum
-ര ׄ⃟🍒˚ .nsfwero
-ര ׄ⃟🍒˚ .nsfwfemdom
-ര ׄ⃟🍒˚ .nsfwglass
-ര ׄ⃟🍒˚ .nsfworgy
-ര ׄ⃟🍒˚ .yuri
-ര ׄ⃟🍒˚ .yuri2
-ര ׄ⃟🍒˚ .yaoi
-ര ׄ⃟🍒˚ .yaoi2
-ര ׄ⃟🍒˚ .panties
-ര ׄ⃟🍒˚ .tetas
-ര ׄ⃟🍒˚ .booty
-ര ׄ⃟🍒˚ .ecchi
-ര ׄ⃟🍒˚ .furro
-ര ׄ⃟🍒˚ .hentai
-ര ׄ⃟🍒˚ .trapito
-ര ׄ⃟🍒˚ .imagenlesbians
-ര ׄ⃟🍒˚ .pene
-ര ׄ⃟🍒˚ .porno
-ര ׄ⃟🍒˚ .pechos
-
-> © mᥱᥒᥙ *ᥒs𝖿ᥕ* ᑲᥡ  ᥴrіss.᥎᥊`.trim();
-
-    conn.sendMessage(m.chat, { image: { url: img }, caption: str, mentions: [m.sender] }, { quoted: fkontak });
-
-await conn.sendMessage(m.chat, { react: { text: '🔥', key: m.key } });
-
-  } catch {
-    conn.reply(m.chat,'*[ ℹ️ ] Error al enviar el menú.*\n\n> ${e}', m);
+> \`\`\`${date} || ${hora}\`\`\`
+`,
+    header: category => `╭──•  𑁯ᰍ   ͘  *\`${category}\`*    ̣  あ  ${xnsfw}  ੭`,
+    body: cmd => `│${xnsfw} ${cmd}`,
+    footer: '╰──•',
+    after: `\n> ${club}`
   }
-};
 
+//--------< 🌹FUNTION🌹 >---------
+  let help = Object.values(global.plugins)
+    .filter(plugin => !plugin.disabled)
+    .map(plugin => ({
+      help: Array.isArray(plugin.help) ? plugin.help : [plugin.help],
+      tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags]
+    }))
+  let groups = {}
+  for (let tag in tags) {
+    groups[tag] = help.filter(plugin => plugin.tags.includes(tag))
+  }
+  let text = [
+    defaultMenu.before,
+    ...Object.keys(tags).map(tag =>
+      [
+        defaultMenu.header(tags[tag]),
+        groups[tag].flatMap(plugin => plugin.help.map(cmd => defaultMenu.body(_p + cmd))).join('\n'),
+        defaultMenu.footer
+      ].join('\n')
+    ),
+    defaultMenu.after
+  ].join('\n')
+//--------< 🌹FUNTION🌹 >---------
+
+  await m.react('🔥')
+  await conn.sendMessage(m.chat, {
+    video: { url: vid },
+    caption: text,
+    mentions: [m.sender],
+    gifPlayback: true
+  }, { quoted: fkontak })
+}
+
+handler.tags = ['main']
 handler.help = ['menunsfw']
 handler.command = /^(menunsfw|comandosnsfw|menuhorny|hornymenu|labiblia|menu18|menu+18|menucaliente|menuporno|pornomenu|menuxxx)$/i;
 handler.fail = null;
 
-export default handler;
+export default handler
