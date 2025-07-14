faf8909f1c9179aa69367ce192c3cd34247a219f
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-13 19:47:28 -0500

    Actualizar emox-rusa.js

--- plugins/emox-rusa.js
+++ plugins/emox-rusa.js
@@ -48,7 +48,7 @@ if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     }
 }
 
-handler.help = ['boobjob/rusa @tag'];
+handler.help = ['rusa'];
 handler.tags = ['emox'];
 handler.command = ['boobjob','rusa'];
 handler.group = true;
