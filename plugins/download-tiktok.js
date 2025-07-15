5c780fc1ab06092a8dedbf8c8943d55526d7ccbb
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-14 22:07:14 -0500

    Actualizar download-tiktok.js

--- plugins/download-tiktok.js
+++ plugins/download-tiktok.js
@@ -70,12 +70,12 @@ let handler = async (m, { conn, text }) => {
 > ${ttwm.title || 'Sin descripción 🍰'}
 
 ▶️ ${ttwm.play_count || 0} | ❤️ ${ttwm.digg_count || 0} | 💬 ${ttwm.comment_count || 0}`
-/*
+
     await conn.sendMessage(m.chat, {
       image: { url: ttwm.author?.avatar },
       caption
-    }, { quoted: m })*/
-    await m.reply(caption)
+    }, { quoted: m })
+    //await m.reply(caption)
 
     if (ttwm.images && ttwm.images.length > 0) {
       const cards = await Promise.all(
