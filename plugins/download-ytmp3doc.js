87497f869158dfc82f5250fb1b98c27b00f57a98
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-15 01:07:03 -0500

    Actualizar download-ytmp3doc.js

--- plugins/download-ytmp3doc.js
+++ plugins/download-ytmp3doc.js
@@ -66,7 +66,7 @@ let handler = async (m, { conn, text }) => {
 };
 
 handler.command = ['ytmp3v2'];
-handler.help = ['ytmp3v2'];
+handler.help = ['ytmp3v2', 'ytmp32'];
 handler.tags = ['download'];
 
 export default handler;
