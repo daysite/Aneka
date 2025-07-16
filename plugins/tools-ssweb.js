315294dd666eec734801f1901dc07ebffc4947fe
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-16 11:40:27 -0500

    Actualizar tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -6,11 +6,19 @@ if (!args[0]) return conn.reply(m.chat, `*${xtools} Por favor, ingresa el Link d
 try {
 await m.react('⌛')
 
+conn.reply(m.chat, '🍭 Buscando su información....', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+
+
 let ss = await (await fetch(https://image.thum.io/get/fullpage/${args[0]})).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
 await m.react('✅')
 } catch {
-return conn.reply(m.chat, '*✖️ Ocurrió un error.*', m)
+return conn.reply(m.chat, '⚠️ Ocurrió un error.', m)
 await m.react(error)}}
 
 handler.help = ['ssweb']
