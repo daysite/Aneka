f4a58158c9d3c186d605e41c91104937d10c1ceb
Dev Criss ☕ <cristianyhacson2008@gmail.com>
2025-07-16 09:57:40 -0500

    Actualizar download-mega.js

--- plugins/download-mega.js
+++ plugins/download-mega.js
@@ -44,7 +44,6 @@ let handler = async (m, { conn, args, usedPrefix, text, command }) => {
 handler.help = ["mega"];
 handler.tags = ["download"];
 handler.command = /^(mega)$/i;
-handler.register = true;
 handler.diamantes = 3;
 
 export default handler;
