27f91c0a10f432d377a6690818bfb51985308bbe
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-19 18:08:57 -0500

    Actualizar download-applemusic.js

--- plugins/download-applemusic.js
+++ plugins/download-applemusic.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Ingresa un nombre de usuario de Instagram\n\n📌 Ejemplo:\n${usedPrefix + command} dev.criss_vx`
+  if (!text) throw `${xtools} Por favor, ingresa un nombre de usuario de Instagram\n📌 Ejemplo:\n${usedPrefix + command} dev.criss_vx`
 
   let res = await fetch(`https://api.vreden.my.id/api/igstalk?query=${encodeURIComponent(text)}`)
   if (!res.ok) throw `🚫 Error al obtener datos del perfil.`
@@ -10,22 +10,23 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   if (!json.result || !json.result.user) throw `⚠️ Usuario no encontrado.`
 
   let user = json.result.user
-  let img = json.result.image || user.hd_profile_pic_url || user.profile_pic_url
+  let img = user.hd_profile_pic_url || user.profile_pic_url
+  //let img = json.result.image || user.hd_profile_pic_url || user.profile_pic_url
   let url = user.external_url || '-'
   let bio = user.biography || '-'
 
   let info = `
-╭━━━[ *🔍 INSTAGRAM STALK* ]
-┃👤 *Usuario:* @${user.username}
-┃📛 *Nombre:* ${user.full_name || '-'}
-┃🧾 *Biografía:* ${bio}
-┃🌐 *Enlace:* ${url}
-┃📦 *Categoría:* ${user.category || 'No definida'}
-┃🖼️ *Publicaciones:* ${user.media_count}
-┃📥 *Seguidores:* ${user.follower_count.toLocaleString()}
-┃📤 *Siguiendo:* ${user.following_count.toLocaleString()}
-┃🔐 *Privado:* ${user.is_private ? 'Sí 🔒' : 'No 🔓'}
-╰━━━━━━━━━━━━━━━━━━━━━━━
+\`\`\`乂 STALKER - INSTAGRAM\`\`\`
+
+*👤 Usuario:* @${user.username}
+*🌴 Nombre:* ${user.full_name || '-'}
+*🍁 Posts:* ${user.media_count}
+*📄 Biografía:* ${bio}
+*⛓️ Enlaces:* ${url}
+*🌵 Categoría:* ${user.category || 'No definida'}
+*👥 Seguidores:* ${user.follower_count.toLocaleString()}
+*📧 Siguiendo:* ${user.following_count.toLocaleString()}
+*🔐 Cuenta:* ${user.is_private ? 'Sí 🔒' : 'No 🔓'}
 `.trim()
 
   await conn.sendFile(m.chat, img, 'perfil.jpg', info, m)
