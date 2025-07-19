81202e86c035392b04440f22198742544cffa2c3
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-19 17:43:24 -0500

    Actualizar search-applemusic.js

--- plugins/search-applemusic.js
+++ plugins/search-applemusic.js
@@ -1,31 +1,4 @@
-import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text }) => {
-if (!text) return m.reply(`Ingresa una petición`)
-
-try {
-let api = await fetch(`https://deliriussapi-oficial.vercel.app/search/applemusic?text=${encodeURIComponent(text)}`)
-let json = await api.json()
-let JT = 'Applemusic  -  Search'
-json.forEach((video, index) => {
-JT += `\n\n`
-JT += `*Nro* : ${index + 1}\n`
-JT += `*Título* : ${video.title}\n`
-JT += `*Tipo* : ${video.type}\n`
-JT += `*Artista* : ${video.artists}\n`
-JT += `*Url* : ${video.url}\n`
-})
-
-await conn.sendFile(m.chat, json[0].image, 'catalogo.jpg', JT, m);
-} catch (error) {
-console.error(error)
-}}
-
-handler.command = /^(applemusicsearch|applemusic)$/i
-
-export default handler
-
-/*
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text, command }) => {
