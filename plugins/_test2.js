e6ab40cfed293b0de853b607f5c2f46dc6fcde96
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-20 10:38:05 -0500

    Actualizar _test2.js

--- plugins/_test2.js
+++ plugins/_test2.js
@@ -1,42 +1,4 @@
-import fetch from 'node-fetch';
 
-const handler = async (m, { args }) => {
-  const query = args.length ? args.join(" ") : "Aesthetic name symbols";
-
-  try {
-    const res = await fetch(`https://delirius-apiofc.vercel.app/tools/symbols?query=${encodeURIComponent(query)}`);
-    if (!res.ok) throw await res.text();
-    const json = await res.json();
-    
-    if (!json.status || !json.data?.symbols?.length) {
-      throw '*⚠️ No se encontraron símbolos para esta búsqueda.*';
-    }
-
-    const symbols = json.data.symbols;
-
-    const randomSymbols = symbols.sort(() => 0.5 - Math.random()).slice(0, 15);
-
-    const message = `
-\`\`\`乂 SIMBOLOS - SEARCH\`\`\`
-
-° *${json.data.query}*
-
-${randomSymbols.map((s, i) => `${s}`).join('\n')}
-
-> ${club}`.trim();
-
-    await m.reply(message);
-  } catch (e) {
-    console.error(e);
-    await m.reply('*⚠️ No se pudieron obtener símbolos para esa búsqueda.*');
-  }
-};
-
-handler.command = /^simbolos|symbols|aesthetic$/i;
-handler.help = ['symbols'];
-handler.tags = ['search'];
-
-export default handler;
 
 
 /*const handler = async (m, { conn, text }) => {
