d91e59d7180f677f16403a3657f384a7a4e4f941
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-19 23:46:39 -0500

    Actualizar plugins

---
+++ plugins/_antiBot.js
@@ -0,0 +1,18 @@
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    if (!m.isGroup) return;
+    let chat = global.db.data.chats[m.chat];
+    let delet = m.key.participant;
+    let bang = m.key.id;
+    let bot = global.db.data.settings[this.user.jid] || {};
+    if (m.fromMe) return true;
+
+    if (m.id.startsWith('3EB0') && m.id.length === 22) {
+        if (chat.antiBot) {
+            await conn.reply(m.chat, `Este grupo no nesesita de tu presencia bot de segunda ⚡`, m);
+            if (isBotAdmin) {
+                await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
+                await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+            }
+        }
+    }
+}
