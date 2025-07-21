1ad0fba1614d89b14cda22321c702abb0e6a1d88
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-20 20:05:48 -0500

    Actualizar gc-kickfantasmas.js

--- plugins/gc-kickfantasmas.js
+++ plugins/gc-kickfantasmas.js
@@ -1,7 +1,19 @@
-const handler = async (m, { conn, args }) => {
+/*const handler = async (m, { conn, args }) => {
 const text = 'daniel es gei y nuv'
 await conn.reply(m.chat, text, m)
 
 handler.command = ['kickfantasmas']
 
-export default handler
+export default handler*/
+
+const handler = async (m, { conn, args }) => {
+  try {
+    const text = 'daniel es...'; // Ajusta el texto según sea necesario
+    await conn.reply(m.chat, text, m);
+  } catch (error) {
+    console.error(error); // Maneja el error según sea necesario
+  }
+};
+
+handler.command = ['kickfantasmas'];
+export default handler;
