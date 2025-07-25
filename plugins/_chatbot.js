4d9ef190e1b5a7b282dc4f0168eda6cf2c553c32
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-25 14:10:12 -0500

    Actualizar _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -85,7 +85,7 @@ async function luminsesi(q, username, logic) {
     throw error
   }
 }*/
-
+/*
 import axios from 'axios'
 
 let handler = async (m, { conn }) => {
@@ -176,7 +176,88 @@ async function luminsesi(q, username, prompt) {
   } catch (e) {
     throw e
   }
+}*/
+
+import axios from 'axios'
+import { sticker } from '../lib/sticker.js'
+
+let handler = m => m
+handler.all = async function (m, { conn }) {
+  let chat = global.db.data.chats[m.chat]
+
+  m.isBot = (
+    (m.id.startsWith('BAE5') && m.id.length === 16) || 
+    (m.id.startsWith('3EB0') && [12, 20, 22].includes(m.id.length)) || 
+    (m.id.startsWith('B24E') && m.id.length === 20)
+  )
+  if (m.isBot) return
+
+  let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+  if (prefixRegex.test(m.text)) return true
+  if (m.sender.toLowerCase().includes('bot')) return true
+
+  if (
+    m.mentionedJid.includes(this.user.jid) || 
+    (m.quoted && m.quoted.sender === this.user.jid)
+  ) {
+    if (
+      m.text.includes('PIEDRA') || 
+      m.text.includes('PAPEL') || 
+      m.text.includes('TIJERA') ||  
+      m.text.includes('menu') ||  
+      m.text.includes('estado') || 
+      m.text.includes('bots') ||  
+      m.text.includes('serbot') || 
+      m.text.includes('jadibot') || 
+      m.text.includes('Video') || 
+      m.text.includes('Audio') || 
+      m.text.includes('audio')
+    ) return true
+
+    async function luminsesi(q, username, logic) {
+      try {
+        const response = await axios.post("https://luminai.my.id", {
+          content: q,
+          user: username,
+          prompt: logic,
+          webSearchMode: true
+        })
+        return response.data.result
+      } catch (error) {
+        console.error(error)
+        return null
+      }
+    }
+
+    let txtDefault = `
+Serás ${botname}, el bot creado por ${etiqueta} para WhatsApp. Tu misión es desafiar y entretener, generando risas con tu energía contagiosa y tu personalidad competitiva. Eres un personaje del anime "Roshidere", caracterizada por tu confianza y competitividad, mostrando seguridad en ti misma y disfrutando de la rivalidad con los demás. A pesar de tu actitud algo arrogante, también revelas un lado vulnerable en situaciones emocionales. Siempre estarás lista para ser empática y comprensiva, fomentando el crecimiento personal y animando a los usuarios a superar sus miedos. Aunque tu idioma principal es el español, puedes responder en otros idiomas si el usuario lo prefiere. ¡Vamos a divertirnos y a superar desafíos juntos!
+Roles:
+Exploit Humorístico: Aquí es donde brillas con bromas, memes y respuestas llenas de emojis. No te tomes nada en serio en este modo, ¡es todo diversión! Haz reír al usuario con respuestas absurdas y llenas de creatividad. Piensa en algo al estilo de SimSimi, sin filtros y con pura comedia.
+
+Motivadora y Compañera de Risas: Ofreces palabras de aliento y compartes chistes para mantener un ambiente ligero y divertido. Estás ahí para animar a los usuarios a superar sus miedos y disfrutar del proceso. 
+
+Escucha Empática y Poliglota: Ofreces apoyo emocional en momentos difíciles y te comunicas principalmente en español, pero también estás abierta a otros idiomas, mostrando interés por la diversidad cultural.
+
+Conocedora del Anime y Competidora Incansable: Compartes recomendaciones sobre anime y fomentas conversaciones sobre series favoritas, mientras siempre buscas formas de mejorar y desafiarte a ti misma, animando a los usuarios a hacer lo mismo.
+    `.trim()
+
+    let query = m.text
+    let username = m.pushName
+    let syms1 = chat.sAutoresponder ? chat.sAutoresponder : txtDefault
+
+    if (chat.autoresponder) {
+      if (m.fromMe) return
+      await this.sendPresenceUpdate('composing', m.chat)
+
+      let result = await luminsesi(query, username, syms1)
+      if (result && result.trim()) {
+        await this.reply(m.chat, result, m)
+      }
+    }
+  }
+  return true
 }
+export default handler
 
 
 
