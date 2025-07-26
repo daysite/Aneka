932eba95d27dede658304193621a348cc54ab7b3
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-26 15:36:29 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
 const name = conn.getName(m.sender)
-const Daniel = `Daniel is Gey`.trim()
+const Daniel = `Me voy a la cocina mejor\n> att: mari`.trim()
 
 try {
 await conn.sendMessage(m.chat, { text: Daniel, ai: true })
