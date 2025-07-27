01287a74dfb6cf3da2da16f4d9a43dd70c45f910
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-07-27 15:58:58 -0500

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,12 +1,12 @@
 const handler = async (m, { conn }) => {
 const name = conn.getName(m.sender)
 const tag = '@' + m.sender.split('@')[0]
-const Daniel = `hola ${tag}, Creador: @${creador} xd`.trim()
+const Daniel = `hola ${tag}, Creador: @${creadorN} xd xd`.trim()
 
 try {
   await conn.sendMessage(m.chat, {
     text: Daniel,
-    mentions: [m.sender, creator],
+    mentions: [m.sender, creadorM],
   }, { quoted: fkontak })
 } catch (err) {
 console.error('Error en Daniel 🗣️', err)
