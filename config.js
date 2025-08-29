080ad0a1c75afefa38962879029f0302d32a1800
daysite <danieldevelop3@gmail.com>
2025-08-29 18:38:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -16,6 +16,8 @@ global.botNumber = '' //Ejemplo: 51927238856
 global.owner = [
   ['50585389943', 'lucxxs.qzy', true],
   ['18294922391',  'Aneka (Colab)', true],
+  //['90812788543600', 'Cris', true], //@lid
+ // ['10342868459625', 'Day Shx!', true], //lid
 ];
 
 //─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─
