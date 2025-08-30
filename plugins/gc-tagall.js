3d41f4131b0ea299d6a69c0e53ff0360bfd9fc34
daysite <danieldevelop3@gmail.com>
2025-08-29 20:58:37 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,9 +8,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
 const oi = `*\`AVISO:\`* ${pesan}`;
-  let teks = `𝗗𝗘𝗦𝗣𝗜𝗘𝗥𝗧𝗘𝗡 𝗣𝗟𝗔𝗡𝗧𝗔𝗦 🌱\n> \`𝖨𝗇𝗍𝖾𝗀𝗋𝖺𝗇𝗍𝖾𝗌:\` *${participants.length}*\n\n ${oi}\n\n  ━━ *_ETIQUETAS_*\n`;
+  let teks = `𝘽𝙪𝙚𝙣𝙤𝙨 𝙙𝙞𝙖𝙨 𝙚𝙨𝙩𝙧𝙚𝙡𝙡𝙞𝙩𝙖𝙨 𝙡𝙖 𝙩𝙞𝙚𝙧𝙧𝙖 𝙡𝙚𝙨 𝙙𝙞𝙘𝙚 𝙝𝙤𝙡𝙖 <3\n> \`𝖨𝗇𝗍𝖾𝗀𝗋𝖺𝗇𝗍𝖾𝗌:\` *${participants.length}*\n\n ${oi}\n\n  ━━ *_ETIQUETAS_*\n`;
   for (const mem of participants) {
-    teks += `യ ׄ🥞˚ @${mem.id.split('@')[0]}\n`;
+    teks += `ֶָ֢⭒🪼 @${mem.id.split('@')[0]}\n`;
   }
   teks += `> ${club}`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
