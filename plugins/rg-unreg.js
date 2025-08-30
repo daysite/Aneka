0901c5be67aaba526fafd93854e87a9fd3b587e3
daysite <danieldevelop3@gmail.com>
2025-08-30 16:55:29 -0500

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `🍭 Tu registro fue eliminado.\n\nGracias por haberte registrado 🙌🏼`, fkontak)
+return conn.reply(m.chat, `🍭 Tu registro fue eliminado.\n\n> Gracias por haberte registrado 🙌🏼`, fkontak)
 
 }
 handler.help = ['unreg']
