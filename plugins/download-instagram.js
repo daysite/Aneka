7264fbb04770eae28f0440d2cf1af04af6d93063
daysite <danieldevelop3@gmail.com>
2025-08-30 19:49:05 -0500

    Update download-instagram.js

--- plugins/download-instagram.js
+++ plugins/download-instagram.js
@@ -2,21 +2,21 @@ import { igdl } from 'ruhend-scraper';
 
 const handler = async (m, { args, conn }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `🎤💙 Por favor, ingresa un enlace de Instagram para descargarlo en el mundo virtual ✨🎵`, m);
+    return conn.reply(m.chat, `🍭 Por favor, ingresa un enlace de Instagram`, m);
   }
 
   try {
-    await m.react('🎤');
+    await m.react('⌛');
     const res = await igdl(args[0]);
     const data = res.data;
 
     for (let media of data) {
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `🎤💙 ¡Aquí tienes tu descarga del concierto virtual! ✨🎵💫`, m);
-    await m.react('✨');
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `🍟Tu reels fue descargado correctamente`, m);
+    await m.react('☑️);
     }
   } catch (e) {
     return conn.reply(m.chat, `🎤💙 ¡Gomen! Ocurrió un error en el mundo virtual. ¡Inténtalo de nuevo! ✨💫`, m);
-    await m.react('💙');
+    await m.react('❎');
   }
 };
 
