55e7e1f3c306c7c0cd35e9f1dc523c7cd7fc8b5a
daysite <danieldevelop3@gmail.com>
2025-09-01 18:06:24 -0500

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -50,11 +50,11 @@ global.error = '✖️'
 
 
 //EMOJIS PREDETERMINADOS
-global.emoji = '🌿'
-global.emoji2 = '🤍'
-global.emoji3 = '☕'
-global.emoji4 = '🍨'
-global.emoji5 = '🍃'
+global.emoji = '🍟'
+global.emoji2 = '🍟'
+global.emoji3 = '🍟'
+global.emoji4 = '🍟'
+global.emoji5 = '🍟'
 global.emojis = [emoji, emoji2, emoji3, emoji4, emoji5].getRandom()
 
 
