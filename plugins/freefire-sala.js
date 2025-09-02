c5ecf890a608efc0144756f5f7687daec0de53a4
daysite <danieldevelop3@gmail.com>
2025-09-01 19:25:53 -0500

    Update freefire-sala.js

--- plugins/freefire-sala.js
+++ plugins/freefire-sala.js
@@ -8,20 +8,20 @@ do b = ps.getRandom()
 while (b === a)
 
   let mensajes = [
-    `*${toM(a)} Vaya preparando esa sala maldita gonorrea. 🙄*`,
-    `*${toM(a)} No te me escondas que esta vez vas a donar la sala 😏*`,
-    `*${toM(a)} ¡La sala mijo yaya! 🗣️*`,
-    `*${toM(a)} La sala cabrón, ¡Ya va a empezar! 👺*`,
-    `*${toM(a)} Donde estás pendejo, ¡Esta vez te toca poner sala a vos! 🫡*`,
-    `*${toM(a)} Pepara esa sala oe gil fast 🗣️*`,
-    `*${toM(a)} ¡Sal del closet, la sala no se pone sola! 😹*`,
-    `*${toM(a)} ¡Deja de dormir! Crea la sala gay 💤*`,
-    `*${toM(a)} ¡Le tocó poner sala a este pobre diablo! 😹*`,
-    `*${toM(a)} ¡Le tocó poner sala al más malo del grupo 🥱😂*`,
-    `*${toM(a)} Le tocó poner sala a esta preciosa, uff mi amor 😍*`,
-    `*${toM(a)} Le tocó poner sala a la más puta del grupo, por cierto cuánto cobras? 😏*`,
-    `*${toM(a)} No tan rápido mijo, Esta vez tu pones la sala o te meto la vrg, elegí. 👿*`,
-    `*${toM(a)} ¡Le tocó poner sala al insano! 💪*`
+    `${toM(a)} Vaya entrando al fifayer te toca poner la sala`,
+    `${toM(a)} No te me escondas que esta vez vas a donar la sala`,
+    `${toM(a)} ¡La sala mijo yaya!`,
+    `${toM(a)} La sala cabrón, ¡Ya va a empezar!`,
+    `${toM(a)} Donde estás pendejo, ¡Esta vez te toca poner sala a vos!`,
+    `${toM(a)} Pepara esa sala oe gil fast`,
+    `${toM(a)} Apurate que te toca poner la sala`,
+    `${toM(a)} ¡Deja de dormir! Crea la sala gei`,
+    `${toM(a)} ¡Le tocó poner sala a este pobre diablo!`,
+    `${toM(a)} ¡Le tocó poner sala al más malo del grupo`,
+    `${toM(a)} Le tocó poner la sala bro`,
+    `${toM(a)} Le tocó poner sala al cachudo del grupo`,
+    `${toM(a)} le toco poner sala a la cachuda del grupo`,
+    `*${toM(a)}¡Le tocó poner sala apurate`
 ]
 
   let mensajeAleatorio = mensajes[Math.floor(Math.random() * mensajes.length)]
