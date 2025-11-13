538cbe978b3425f1976f7d8a3c1879f78a7ea68e
daysite <danieldevelop3@gmail.com>
2025-11-12 19:03:44 -0500

    Update gc-promote.js

--- plugins/gc-promote.js
+++ plugins/gc-promote.js
@@ -1,34 +1,23 @@
-const handler = async (m, {conn, usedPrefix, text}) => {
+var handler = async (m, { conn, usedPrefix, command, text, groupMetadata, isAdmin }) => {
+let mentionedJid = await m.mentionedJid
+let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
+if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder promoverlo a administrador.`, m)
+try {
+const groupInfo = await conn.groupMetadata(m.chat)
+const ownerGroup = groupInfo.owner || m.chat.split('-')[0] + '@s.whatsapp.net'
+if (user === ownerGroup || groupInfo.participants.some(p => p.id === user && p.admin))
+return conn.reply(m.chat, 'ꕥ El usuario mencionado ya tiene privilegios de administrador.', m)
+await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
+await conn.reply(m.chat, `❀ Fue agregado como admin del grupo con exito.`, m)
+} catch (e) {
+conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
+}}
 
-  if (isNaN(text) && !text.match(/@/g)) {
+handler.help = ['promote']
+handler.tags = ['grupo']
+handler.command = ['promote', 'promover']
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
 
-  } else if (isNaN(text)) {
-    var number = text.split`@`[1];
-  } else if (!isNaN(text)) {
-    var number = text;
-  }
-
-  if (!text && !m.quoted) return conn.reply(m.chat, `${xgc} Menciona algún participante`, m);
-  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, '🍭 El usuario ingresado es incorrecto.', m);
-
-  try {
-    if (text) {
-      var user = number + '@s.whatsapp.net';
-    } else if (m.quoted.sender) {
-      var user = m.quoted.sender;
-    } else if (m.mentionedJid) {
-      var user = number + '@s.whatsapp.net';
-    }
-  } catch (e) {
-  } finally {
-    conn.groupParticipantsUpdate(m.chat, [user], 'promote');
-    conn.reply(m.chat, `🍟 El usuario fue promovido a administrador`, m);
-  }
-};
-handler.help = ['promote'];
-handler.tags = ['gc'];
-handler.command = /^(promote|promover|daradmin|darpoder|darpija|darteta|dartt|dartta)$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-export default handler;
+export default handler
